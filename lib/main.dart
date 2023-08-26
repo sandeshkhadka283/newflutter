fc84cda00591ad78bb848b5b32a43513f9e3c06a
Sandesh Khadka <ksandesh283@gmail.com>
2023-08-26 19:12:52 +0545

    some minor UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
-  runApp(const PokemonApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: PokemonApp(),
+  ));
 }
 
 class PokemonApp extends StatelessWidget {
-  const PokemonApp({super.key});
+  const PokemonApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +33,7 @@ class Pokemon {
 }
 
 class PokemonListScreen extends StatefulWidget {
-  const PokemonListScreen({super.key});
+  const PokemonListScreen({Key? key}) : super(key: key);
 
   @override
   _PokemonListScreenState createState() => _PokemonListScreenState();
@@ -83,80 +86,106 @@ class _PokemonListScreenState extends State<PokemonListScreen> {
     });
   }
 
+  int calculateCrossAxisCount(double screenWidth) {
+    if (screenWidth >= 600) {
+      return 3; // Use 3 columns on larger screens
+    } else if (screenWidth >= 400) {
+      return 2; // Use 2 columns on medium screens
+    } else {
+      return 1; // Use 1 column on small screens
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                onChanged: (value) => filterPokemon(value.toLowerCase()),
-                style:
-                    const TextStyle(color: Colors.black), // Change text color
-                decoration: InputDecoration(
-                  labelText: 'Search Pokemon',
-                  labelStyle:
-                      const TextStyle(color: Colors.black, fontSize: 12),
-                  prefixIcon: const Icon(Icons.search,
-                      color: Colors.black), // Change icon color
-                  filled: true,
-                  fillColor: Theme.of(context)
-                      .primaryColor
-                      .withOpacity(0.05), // Change field background color
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none, // Hide border
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(
-                        color: Theme.of(context)
-                            .primaryColor), // Change border color
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            final screenWidth = constraints.maxWidth;
+
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(60.0),
+                  child: TextField(
+                    onChanged: (value) => filterPokemon(value.toLowerCase()),
+                    style: const TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      labelText: 'Search Pokemon',
+                      labelStyle:
+                          const TextStyle(color: Colors.black, fontSize: 12),
+                      prefixIcon: const Icon(Icons.search, color: Colors.black),
+                      filled: true,
+                      fillColor:
+                          Theme.of(context).primaryColor.withOpacity(0.05),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide:
+                            BorderSide(color: Theme.of(context).primaryColor),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            isLoading
-                ? Center(
-                    child: CircularProgressIndicator(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  )
-                : Expanded(
-                    child: ListView.builder(
-                      itemCount: displayedPokemon.length,
-                      itemBuilder: (context, index) {
-                        final pokemon = displayedPokemon[index];
-                        return InkWell(
-                          onTap: () {},
-                          child: Card(
-                            
-                            elevation: 2,
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 8),
-                            child: ListTile(
-                              leading: Image.network(pokemon.imageUrl),
-                              title: Text(
-                                pokemon.name.toUpperCase(),
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              subtitle: Text(
-                                pokemon.types.join(', '),
-                                style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ),
+                isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )
+                    : Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount:
+                                calculateCrossAxisCount(screenWidth),
+                            childAspectRatio: screenWidth < 600
+                                ? 2 // Adjust aspect ratio for mobile
+                                : 4, // Adjust aspect ratio for desktop
                           ),
-                        );
-                      },
-                    ),
-                  ),
-          ],
+                          itemCount: displayedPokemon.length,
+                          itemBuilder: (context, index) {
+                            final pokemon = displayedPokemon[index];
+                            return InkWell(
+                              onTap: () {},
+                              child: Card(
+                                elevation: 2,
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 8),
+                                child: Container(
+                                  constraints: BoxConstraints(
+                                    maxHeight: screenWidth < 600
+                                        ? double.infinity
+                                        : 150, // Set maximum card height here
+                                  ),
+                                  child: ListTile(
+                                    leading: Image.network(pokemon.imageUrl),
+                                    title: Text(
+                                      pokemon.name.toUpperCase(),
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      pokemon.types.join(', '),
+                                      style: TextStyle(
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+              ],
+            );
+          },
         ),
       ),
     );
